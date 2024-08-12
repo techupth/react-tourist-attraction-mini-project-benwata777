a97e6554cc822dad319b2ee5b38b805e553fd946
benwata777 <benwata777@gmail.com>
2024-08-12 18:13:53 +0700

    fix

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import trips from "./db.js";
 
 const app = express();
-const port = 4001;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
